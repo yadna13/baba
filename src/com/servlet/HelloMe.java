3a80363fba3a56f9986c71c1d0efabde0f88e1f5
Pavan Kumar Suryanarayana Rao <pavankumar19912000@yahoo.com>
2015-07-14 14:01:04 -0700

    Changes

--- src/com/servlet/HelloMe.java
+++ src/com/servlet/HelloMe.java
@@ -6,31 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloMe
- */
 public class HelloMe extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+    
     public HelloMe() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//Hello, This is the change
 	}
 
 }
